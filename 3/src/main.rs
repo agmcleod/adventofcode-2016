4e0cf8bee1a5f65a832f5619f51206d00386b40f
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-12-03 13:14:13 -0500

    3 p1 working

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,4 +1,6 @@
 extern crate read_input;
+extern crate regex;
+use regex::Regex;
 
 fn main() {
     let text = match read_input::read_text("input.txt") {
@@ -6,5 +8,20 @@ fn main() {
         Err(err) => panic!("{:?}", err),
     };
 
+    let lines = text.split("\n");
+    let mut count = 0;
+    let split_re = Regex::new(r"\s+").unwrap();
+    for line in lines {
+        if line == "" {
+            continue
+        }
 
+        let mut numbers: Vec<usize> = split_re.split(line.trim()).map(|v| v.parse().unwrap()).collect();
+        numbers.sort();
+        if numbers[0] + numbers[1] > numbers[2] {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
 }
