b39e80e68adaef93dfbcb0efb3b159ae87da5912
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-12-12 20:57:03 -0500

    Working on operating bot instructions

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -21,6 +21,10 @@ impl Bot {
         }
     }
 
+    fn has_two(self: &Bot) -> bool {
+        self.chips[0] != 0 && self.chips[1] != 0
+    }
+
     fn take_chip(self: &mut Bot, which: &str) -> usize {
         let mut return_value = 0;
         if which == "low" {
@@ -45,24 +49,30 @@ impl Bot {
     }
 }
 
-struct Instruction {
+struct Instruction<'a> {
     low_index: usize,
     high_index: usize,
+    low_type: &'a str,
+    high_type: &'a str,
 }
 
-impl Instruction {
-    fn new(low_index: usize, high_index: usize) -> Instruction {
-        Instruction{ low_index: low_index, high_index: high_index }
+impl<'a> Instruction<'a> {
+    fn new(low_index: usize, high_index: usize, low_type: &'a str, high_type: &'a str) -> Instruction<'a> {
+        Instruction{ low_index: low_index, high_index: high_index, low_type: low_type, high_type: high_type }
     }
 }
 
-fn give_to_bot(bots: &mut HashMap<usize, Bot>, index: &usize, value: usize) {
+type BotMap = HashMap<usize, Bot>;
+type InstructionMap<'a> = HashMap<usize, Instruction<'a>>;
+type OutputMap = HashMap<usize, [usize; 20]>;
+
+fn give_to_bot(bots: &mut BotMap, index: &usize, value: usize) {
     let mut insert_new = false;
     match bots.get_mut(index) {
-        Some(receiver) => receiver.give_chip(value),
-        None => {
-            insert_new = true;
+        Some(receiver) => {
+            receiver.give_chip(value);
         },
+        None => insert_new = true,
     }
 
     if insert_new {
@@ -70,7 +80,7 @@ fn give_to_bot(bots: &mut HashMap<usize, Bot>, index: &usize, value: usize) {
     }
 }
 
-fn map_bots(bots: &mut HashMap<usize, Bot>, text: &String) {
+fn map_bots(bots: &mut BotMap, text: &String) {
     for line in text.lines() {
         let mut words = line.split(" ");
         let line_type = words.next();
@@ -89,13 +99,13 @@ fn map_bots(bots: &mut HashMap<usize, Bot>, text: &String) {
     }
 }
 
-fn new_bot_with_chip(bots: &mut HashMap<usize, Bot>, index: usize, num: usize) {
+fn new_bot_with_chip(bots: &mut BotMap, index: usize, num: usize) {
     let mut bot = Bot::new();
     bot.give_chip(num);
     bots.insert(index, bot);
 }
 
-fn map_instructions(instructions: &mut HashMap<usize, Instruction>, text: &String) {
+fn map_instructions<'a>(instructions: &'a mut InstructionMap<'a>, outputs: &'a mut OutputMap, text: &'a String) {
     for line in text.lines() {
         let mut words = line.split(" ");
         let line_type = words.next();
@@ -105,12 +115,26 @@ fn map_instructions(instructions: &mut HashMap<usize, Instruction>, text: &Strin
                 None => panic!("No value found for instruction index"),
             };
 
-            let low_index: usize = match words.nth(4) {
+            let low_type = match words.nth(3) {
+                Some(t) => t,
+                None => panic!("No value found for low type (bot|output)"),
+            };
+
+            let low_index: usize = match words.next() {
                 Some(n) => n.parse().expect("low value should be a number"),
                 None => panic!("No value found for \"low give to\" index"),
             };
 
-            let high_index: usize = match words.nth(4) {
+            if low_type == "output" {
+                outputs.insert(low_index, [0; 20]);
+            }
+
+            let high_type = match words.nth(3) {
+                Some(t) => t,
+                None => panic!("No value found for high type (bot|output)"),
+            };
+
+            let high_index: usize = match words.next() {
                 Some(n) => n.parse().expect("high value should be a number"),
                 None => panic!("No value found for \"high give to\" index"),
             };
@@ -118,7 +142,57 @@ fn map_instructions(instructions: &mut HashMap<usize, Instruction>, text: &Strin
             if instructions.contains_key(&index) {
                 panic!("Instruction already contains one for bot {}", index);
             }
-            instructions.insert(index, Instruction::new(low_index, high_index));
+            instructions.insert(index, Instruction::new(low_index, high_index, low_type, high_type));
+        }
+    }
+}
+
+fn insert_chip_into_output(output: &mut[usize; 20], val: usize) {
+    let mut it = output.iter_mut();
+    loop {
+        match it.next() {
+            Some(n) => {
+                if *n == 0 {
+                    *n = val;
+                    break
+                }
+            },
+            None => break
+        }
+    }
+}
+
+fn operate_two_chip_bots(bots: &mut BotMap, outputs: &mut OutputMap, instructions: &InstructionMap) {
+    let mut bot_operations: Vec<[usize; 2]> = Vec::with_capacity(bots.len());
+    for (giver_index, bot) in bots.iter_mut() {
+        if bot.has_two() {
+            let low = bot.take_chip("low");
+            let high = bot.take_chip("high");
+            let instruction = match instructions.get(giver_index) {
+                Some(instruction) => instruction,
+                None => panic!("No instruction for {}", giver_index),
+            };
+            if instruction.low_type == "bot" {
+                bot_operations.push([instruction.low_index, low]);
+            } else {
+                println!("{}, {:?}", instruction.low_index, outputs);
+                let mut output = outputs.get_mut(&instruction.low_index).unwrap();
+                insert_chip_into_output(&mut output, low);
+            }
+
+            if instruction.high_type == "bot" {
+                bot_operations.push([instruction.high_index, high]);
+            } else {
+                println!("{}, {:?}", instruction.high_index, outputs);
+                let mut output = outputs.get_mut(&instruction.high_index).unwrap();
+                insert_chip_into_output(&mut output, low);
+            }
+        }
+    }
+
+    for operation in bot_operations {
+        if operation[0] != 0 && operation[1] != 0 {
+            give_to_bot(bots, &operation[0], operation[1]);
         }
     }
 }
@@ -129,8 +203,26 @@ fn main() {
         Err(e) => panic!("{:?}", e),
     };
 
-    let mut bots: HashMap<usize, Bot> = HashMap::new();
-    let mut instructions: HashMap<usize, Instruction> = HashMap::new();
+    let mut bots: BotMap = HashMap::new();
+    let mut outputs: OutputMap = HashMap::new();
+    let mut instructions: InstructionMap = HashMap::new();
     map_bots(&mut bots, &text);
-    map_instructions(&mut instructions, &text);
+    map_instructions(&mut instructions, &mut outputs, &text);
+
+    let target_values = [61, 17];
+    let mut found_bot = false;
+    loop {
+        operate_two_chip_bots(&mut bots, &mut outputs, &instructions);
+        for (i, bot) in bots.iter_mut() {
+            if (bot.chips[0] == target_values[0] && bot.chips[1] == target_values[1]) || (bot.chips[0] == target_values[1] && bot.chips[1] == target_values[0]) {
+                println!("{}", i);
+                found_bot = true;
+                break
+            }
+        }
+
+        if found_bot {
+            break
+        }
+    }
 }
