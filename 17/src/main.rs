2d0e7004925c93e2647f43f05788eb3290271c11
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-05-14 15:38:24 -0400

    part 1 of day17

--- 17/src/main.rs
+++ 17/src/main.rs
@@ -39,51 +39,60 @@ fn get_md5(md5: &mut Md5, v: &String) -> String {
     result
 }
 
-fn try_position(door_letters: &[&str; 4], options_based_on_position: &HashMap<(usize, usize), [Option<(usize, usize)>; GRID_SIZE]>, input: String, md5: &mut Md5, position: (usize, usize), path: Vec<&str>) -> bool {
-    let next_value = get_md5(md5, &input);
+struct State<'a> {
+    path: Vec<&'a str>,
+    position: (usize, usize),
+    input: String,
+}
 
-    let positions = options_based_on_position.get(&(position.0, position.1)).unwrap();
-    let mut not_finished = true;
-    for (i, ch) in next_value[0..GRID_SIZE].chars().enumerate() {
-        if door_is_open(&ch) {
-            let position = positions[i];
-            match position {
-                Some(p) => {
-                    if p.0 == GRID_SIZE - 1 && p.1 == GRID_SIZE {
-                        not_finished = false;
-                        break
-                    } else {
-                        let new_input = format!("{}{}", input, door_letters[i]);
-                        let mut appended_path = path.clone();
-                        appended_path.push(door_letters[i]);
-                        not_finished = try_position(&door_letters, &options_based_on_position, new_input, md5, p, appended_path);
-                    }
-                },
-                None => continue,
+fn try_position(door_letters: &[&str; 4], options_based_on_position: &HashMap<(usize, usize), [Option<(usize, usize)>; GRID_SIZE]>) {
+    let mut md5 = Md5::new();
+
+    let mut options = vec![State{ path: Vec::new(), position: (0, 0), input: String::from("qtetzkpl") }];
+    loop {
+        let mut new_options: Vec<State> = Vec::with_capacity(options.len() * 4);
+        let mut finished = false;
+        for state in &options {
+            let next_value = get_md5(&mut md5, &state.input);
+            let position = &state.position;
+            if position.0 == GRID_SIZE - 1 && position.1 == GRID_SIZE - 1 {
+                finished = true;
+                println!("{} {}", state.path.join(""), state.path.len());
+                break
             }
+            let positions = options_based_on_position.get(&(position.0, position.1)).unwrap();
+            for (i, ch) in next_value[0..GRID_SIZE].chars().enumerate() {
+                if door_is_open(&ch) {
+                    let position = positions[i];
+                    match position {
+                        Some(p) => {
+                            let mut appended_path = state.path.clone();
+                            appended_path.push(door_letters[i]);
+                            new_options.push(State{ path: appended_path, position: p, input: format!("{}{}", state.input, door_letters[i]) });
+                        },
+                        None => continue,
+                    }
 
+                }
+            }
+        }
+
+        if !new_options.len() > 0 {
+            options = new_options;
+        }
+
+        if finished {
+            break
         }
     }
-
-    if !not_finished {
-        println!("{:?}", path);
-    }
-
-    not_finished
 }
 
 fn main() {
-    // x, y, or column, row
-    let position = (0, 0);
-    let input = String::from("qtetzkpl");
-    let mut md5 = Md5::new();
-
     let mut options_based_on_position = HashMap::<(usize, usize), [Option<(usize, usize)>; GRID_SIZE]>::new();
 
     let door_letters = ["U", "D", "L", "R"];
 
     build_options(&mut options_based_on_position);
 
-    let path = Vec::<&str>::with_capacity(16);
-    try_position(&door_letters, &options_based_on_position, input, &mut md5, position, path);
+    try_position(&door_letters, &options_based_on_position);
 }
